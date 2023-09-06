79009089cef066154644e833098fb50c8f242e1d
Ahren <205530972@qq.com>
2023-09-07 07:13:35 +0800

    1.0.1.7

--- SceneServer/Scripts/manager/game_manager.lua
+++ SceneServer/Scripts/manager/game_manager.lua
@@ -2,21 +2,25 @@
 
 local m = {}
 m.ScriptLoadedEvent = Event.newEvent()
----comment
+
+---lua 脚本加载完成回调
+---@param game any
 function m.ScriptLoaded(game)
 
     m.ScriptLoadedEvent:trigger(game)
 
     log("Lua ScriptLoaded...")
-    --ItemUpgrade.Init()
-    --PlayerManager.Init()
-    --GlobalTimerManager.Init()
-    --PointTriggerMnager.Init();
 
-    -- package.cpath = package.cpath .. ";c:/Users/Administrator/.vscode/extensions/tangzx.emmylua-0.5.17/debugger/emmy/windows/x64/?.dll"
-    -- local dbg = require("emmy_core")
-    -- dbg.tcpListen("localhost", 9966)
 end
+
+---游戏启动完成事件
+---@param game Game
+function m.GameStartedEvent(game)
+
+
+end
+
+
 
 GameManager = m
 return m
